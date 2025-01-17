98cec4f99d255d138d3888ae89cf96f36772d783
Acrylic125 <acrylic125email@gmail.com>
2025-01-17 16:12:02 +0800

    feat: lab 1

---
+++ sc1008/week 2/lab_p1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    while (1) {
+        int studentId;
+        printf("Student Id:\n\r");
+        scanf("%d", &studentId);
+        if (studentId == -1) {
+            return 0;
+        }
+
+        int mark;
+        printf("Mark:\n\r");
+        scanf("%d", &mark);
+
+        if (mark < 0 || mark > 100) {
+            printf("%d is not a valid mark.", mark);
+            continue;
+        }
+        
+        char grade = ' ';
+        if (mark <= 44) {
+            grade = 'F';
+        } else if (mark <= 54) {
+            grade = 'D';
+        } else if (mark <= 64) {
+            grade = 'C';
+        } else if (mark <= 74) {
+            grade = 'B';
+        } else {
+            grade = 'A';
+        }
+
+        printf("Grade %c\n\r", grade);
+    }
+    return 0;
+}
---
+++ sc1008/week 2/lab_p2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int lines;
+    printf("Enter number of lines: \n\r");
+    scanf("%d", &lines);
+
+    for (int i = 0; i < lines; i++) {
+        printf("Enter line %d (end with -1): \n\r", i + 1);
+        float tally = 0;
+        int count = 0;
+        
+        while (1) {
+            int num;
+            scanf("%d", &num);
+
+            if (num == -1) {
+                break;
+            }
+
+            if (num < 0) {
+                continue;
+            }
+            count++;
+            tally += num;
+        }
+
+        float average = tally / count;
+        printf("Average: %.2f\n\r", average);
+    }
+    return 0;
+}
